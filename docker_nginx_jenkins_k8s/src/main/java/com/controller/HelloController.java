c9bf64d6c1c36c8b1ec3208226a8b380b1e7510b
neverMindHins <kocotam@qq.com>
2023-12-11 16:23:59 +0800

    project first to commit

---
+++ docker_nginx_jenkins_k8s/src/main/java/com/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello, World!";
+    }
+}
